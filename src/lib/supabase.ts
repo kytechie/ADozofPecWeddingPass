eb9caa40ae6b2bace38d196e812045e43bd0717d
Peculiar Chard <peculiarugbo@gmail.com>
2026-08-25 12:15:56 +0100

    Force fresh deployment

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-console.log("CURRENT SUPABASE URL:", supabaseUrl);
+console.log("CURRENT SUPABASE URL v2:", supabaseUrl);
 
 export const supabase = createClient(
   supabaseUrl,
